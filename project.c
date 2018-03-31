7d1686b94edac6f98d397759ff2fac5f2d0093c6
sauravgupta2800 <sauravgupta2800@gmail.com>
2018-03-31 09:48:40 +0530

    second commit

--- project.c
+++ project.c
@@ -1,4 +1,21 @@
 #include<stdio.h>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+#define MAX 1000
+#define QUANTA 4
+
+int flag[MAX],at[MAX],bt[MAX],pt[MAX],rt[MAX],ft[MAX],fe[MAX],fe_flag[MAX],pid[MAX],tms,qt[MAX];
+
+//at arrival time
+//bt burst time
+//rt Response Time
+//pt priority
+//pid process id
+//difining modules
+
+queue<int> q;  //RR queue
 int main()
 {
 
