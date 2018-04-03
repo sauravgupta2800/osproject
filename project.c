09a7ecce5583969377ca25a1275edeaa251b4ddb
sauravgupta2800 <sauravgupta2800@gmail.com>
2018-04-03 22:29:04 +0530

    commit third

--- project.c
+++ project.c
@@ -13,9 +13,31 @@ int flag[MAX],at[MAX],bt[MAX],pt[MAX],rt[MAX],ft[MAX],fe[MAX],fe_flag[MAX],pid[M
 //rt Response Time
 //pt priority
 //pid process id
-//difining modules
 
 queue<int> q;  //RR queue
+
+void RR()
+{
+      if(!q.empty())
+      {
+      	if(rt[q.front()]>0 && qt[q.front()]<4)
+      	{
+          		qt[q.front()]++;
+          		rt[q.front()]--;
+          		if(rt[q.front()]==0)
+          		{
+            	ft[q.front()]=tms+1;
+            	q.pop();
+          		}
+          		if(rt[q.front()]!=0 && qt[q.front()]==4)
+          		{
+				qt[q.front()]=0;
+				q.push(q.front());
+				q.pop();
+          		}
+        	}
+      }
+}
 int main()
 {
 
